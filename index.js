895526a34c0725ed769d482b9df01569c6e769dc
Daniel Magers <daniel.magers@gmail.com>
2022-06-10 16:17:43 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+   let newArray = [];
+    for (let i = 0; i < names.length; i++) {
+newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+return newArray;
+}
+
+const integer = [];
+function countDown(integer) {
+  let i = 10;
+  while (i >=0) {
+    console.log(i--);
+  };
+  return integer;
+}
+console.log(countDown());
+
